160cb93fbb6516a87843699a235b003b69f4c08c
Roy v.Oldenbeek <roy_v_oldenbeek@hotmail.com>
2016-06-23 09:58:15 +0200

    Database geupdate met insert scripts

--- Database/Bootverhuur.sql
+++ Database/Bootverhuur.sql
@@ -13,10 +13,14 @@ drop sequence ai_huur;
 drop sequence ai_materiaal;
 drop sequence ai_vaarwater;
 
-create sequence ai_klant;
-create sequence ai_huur;
-create sequence ai_materiaal;
-create sequence ai_vaarwater;
+create sequence ai_klant
+START WITH 100;
+create sequence ai_huur
+START WITH 100;
+create sequence ai_materiaal
+START WITH 100;
+create sequence ai_vaarwater
+START WITH 100;
 
 CREATE TABLE KLANT(
 KlantID NUMBER PRIMARY KEY,
@@ -105,3 +109,45 @@ BEGIN
 :new.VaarwaterID := ai_vaarwater.NEXTVAL;
 END;
 /
+
+-- INSERTS --
+
+--Vaarwater Inserts--
+INSERT INTO VAARWATER(Naam) VALUES('IJsselmeer');
+INSERT INTO VAARWATER(Naam) VALUES('Noordzee');
+
+--Type Inserts--
+INSERT INTO TYPE(Type,Omschrijving) VALUES ('Motorboot','Dit is een boot die aangedreven wordt door een motor');
+INSERT INTO TYPE(Type,Omschrijving) VALUES ('Spierkrachtboot', 'Dit is een boot die aangedreven wordt door spierkracht of wind');
+
+-- TypeVaarwater Inserts--
+INSERT INTO TypeVaarwater(VaarwaterID,Type) VALUES(100,'Motorboot');
+INSERT INTO TypeVaarwater(VaarwaterID,Type) VALUES(101,'Motorboot');
+
+--Boot Inserts--
+INSERT INTO Boot(Naam,Type,Literinhoud,Klasse) VALUES('Snelboot','Motorboot',15,'Kruiser');
+INSERT INTO Boot(Naam,Type,Literinhoud,Klasse) VALUES('Niet zo snel boot','Motorboot',20,'Kruiser');
+INSERT INTO Boot(Naam,Type,Literinhoud,Klasse) VALUES('Ook een snelle boot','Motorboot',15,'Kruiser');
+INSERT INTO Boot(Naam,Type,Literinhoud,Klasse) VALUES('Langvaarboot','Motorboot',50,'Kruiser');
+INSERT INTO BOOT(Naam,Type,Klasse) VALUES('Langzameboot','Spierkrachtboot','Valk');
+INSERT INTO BOOT(Naam,Type,Klasse) VALUES('De Horizon','Spierkrachtboot','Valk');
+INSERT INTO BOOT(Naam,Type,Klasse) VALUES('De Zonsondergang','Spierkrachtboot','Valk');
+INSERT INTO BOOT(Naam,Type,Klasse) VALUES('De Dageraad','Spierkrachtboot','Valk');
+INSERT INTO BOOT(Naam,Type,Klasse) VALUES('Blauw licht','Spierkrachtboot','Laser');
+INSERT INTO BOOT(Naam,Type,Klasse) VALUES('Rood licht','Spierkrachtboot','Laser');
+INSERT INTO BOOT(Naam,Type,Klasse) VALUES('Groen licht','Spierkrachtboot','Laser');
+INSERT INTO BOOT(Naam,Type,Klasse) VALUES('Roei tot je een ons weegt','Spierkrachtboot','Kano');
+INSERT INTO BOOT(Naam,Type,Klasse) VALUES('Twee peddels extra','Spierkrachtboot','Kano');
+INSERT INTO BOOT(Naam,Type,Klasse) VALUES('Twee peddels minder','Spierkrachtboot','Kano');
+
+--Materiaal Insert--
+INSERT INTO MATERIAAL(Naam, Omschrijving) VALUES('SlaapPakket','Dit pakket bestaat uit een luchtbed en een slaapzak');
+INSERT INTO MATERIAAL(Naam, Omschrijving) VALUES('KookPakket','Met dit pakket kun je aan wal koken');
+INSERT INTO MATERIAAL(Naam, Omschrijving) VALUES('Zaklamp','Schijnt tot 100m ver!');
+INSERT INTO MATERIAAL(Naam, Omschrijving) VALUES('Zwemvest','Zodat je niet verdrinkt!');
+INSERT INTO MATERIAAL(Naam, Omschrijving, Pakket) VALUES('Luchtbed','Om op te slapen',100);
+INSERT INTO MATERIAAL(Naam, Omschrijving, Pakket) VALUES('Slaapzak','Om in te slapen',100);
+INSERT INTO MATERIAAL(Naam, Omschrijving, Pakket) VALUES('Gaspit','Om op te koken',101);
+INSERT INTO MATERIAAL(Naam, Omschrijving, Pakket) VALUES('Gasfles','Inclusief gas! Om mee te koken',101);
+INSERT INTO MATERIAAL(Naam, Omschrijving, Pakket) VALUES('Koelbox','Dan blijft je eten lekker vers!',101);
+INSERT INTO MATERIAAL(Naam, Omschrijving, Pakket) VALUES('Servies','Nu kun je ook ergens op eten',101);
